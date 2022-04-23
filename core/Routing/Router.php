e20202204169ed7a4a343e60d3fd37d7062eb3fb
Bakhtiyor <alijanovbakhtiyor@gmail.com>
2022-04-23 12:32:22 +0500

    initial routing

--- core/Routing/Router.php
+++ core/Routing/Router.php
@@ -1,10 +1,28 @@
 <?php
 
+namespace Core\Routing;
+
 class Router
 {
-    public function get(string $route, $handler)
-    {
+    protected string $incomeRoute;
+    protected string $method;
 
+    protected array $routes = [
+        'GET' => []
+    ];
+
+    public function get(string $route, callable $handler)
+    {
+        $this->routes['GET'][$route] = $handler;
+    }
+
+    public function processRequest($route, $method)
+    {
+        if (array_key_exists($route, $this->routes[$method])) {
+            return call_user_func($this->routes[$method][$route]);
+        }
+
+        return '404';
     }
 
 }
--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
 
-echo '<pre>';
-print_r($_SERVER);
+include 'vendor/autoload.php';
+
+use Core\Routing\Router;
+
+$router = new Router();
+
+$router->get('/', function () {
+    return 'some data';
+});
+
+$router->get('/name',function (){
+    return 'my name';
+});
+
+echo $router->processRequest($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
 
